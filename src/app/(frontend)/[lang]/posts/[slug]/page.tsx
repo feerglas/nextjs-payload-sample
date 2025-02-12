585e7e944b99bdc18d96ca841514efde9e287a20
Yves Tscherry <info@feerglas.com>
2025-02-12 09:50:42 +0100

    translate post item back link

--- src/app/(frontend)/[lang]/posts/[slug]/page.tsx
+++ src/app/(frontend)/[lang]/posts/[slug]/page.tsx
@@ -8,13 +8,14 @@ import { draftMode } from 'next/headers'
 import React, { cache } from 'react'
 import RichText from '@/components/RichText'
 
-import type { Post, Config } from '@/payload-types'
+import type { Post, Config, Pageglobal } from '@/payload-types'
 
 import { PostHero } from '@/heros/PostHero'
 import { generateMeta } from '@/utilities/generateMeta'
 import PageClient from './page.client'
 import { LivePreviewListener } from '@/components/LivePreviewListener'
 import Link from 'next/link'
+import { getCachedGlobal } from '@/utilities/getGlobals'
 
 export async function generateStaticParams() {
   const payload = await getPayload({ config: configPromise })
@@ -62,37 +63,38 @@ export default async function Post({ params: paramsPromise }: Args) {
   const { slug = '', lang } = await paramsPromise
   const url = '/posts/' + slug
   const post = await queryPostBySlug({ slug, lang })
+  const globalsData: Pageglobal = await getCachedGlobal('pageglobal', 1, lang)()
 
   if (!post) return <PayloadRedirects url={url} />
 
   return (
     <div>
-    <div className="container mx-auto px-5">
-      <PageClient />
+      <div className="container mx-auto px-5 mb-20">
+        <PageClient />
 
-      {/* Allows redirects for valid pages too */}
-      <PayloadRedirects disableNotFound url={url} />
+        {/* Allows redirects for valid pages too */}
+        <PayloadRedirects disableNotFound url={url} />
 
-      {draft && <LivePreviewListener />}
+        {draft && <LivePreviewListener />}
 
-      <h2 className="mb-16 mt-10 text-2xl font-bold leading-tight tracking-tight md:text-4xl md:tracking-tighter">
-        <Link href={`/${lang}`} className="hover:underline">
-          SAGW News
-        </Link>
-      </h2>
+        <h2 className="mb-16 mt-10 text-2xl font-bold leading-tight tracking-tight md:text-4xl md:tracking-tighter">
+          <Link href={`/${lang}`} className="hover:underline">
+            {globalsData.postsTitle}
+          </Link>
+        </h2>
 
-      <PostHero post={post} lang={lang} />
+        <PostHero post={post} lang={lang} />
 
-      <div className="container mx-auto px-5">
-      <RichText className="mx-auto max-w-2xl" data={post.content} enableGutter={false} />
-        {post.relatedPosts && post.relatedPosts.length > 0 && (
-          <RelatedPosts
-            className="mt-12 max-w-[52rem] lg:grid lg:grid-cols-subgrid col-start-1 col-span-3 grid-rows-[2fr]"
-            docs={post.relatedPosts.filter((post) => typeof post === 'object')}
-          />
-        )}
+        <div className="container mx-auto px-5">
+        <RichText className="mx-auto max-w-2xl" data={post.content} enableGutter={false} />
+          {post.relatedPosts && post.relatedPosts.length > 0 && (
+            <RelatedPosts
+              className="mt-12 max-w-[52rem] lg:grid lg:grid-cols-subgrid col-start-1 col-span-3 grid-rows-[2fr]"
+              docs={post.relatedPosts.filter((post) => typeof post === 'object')}
+            />
+          )}
+        </div>
       </div>
-    </div>
     </div>
   )
 }
