dbb8319d4dae028b316fb9f564affb820b53d761
Yves Tscherry <info@feerglas.com>
2025-02-14 08:01:47 +0100

    set loading state on button

--- src/components/GenerateSummary/index.tsx
+++ src/components/GenerateSummary/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import React from "react";
 import { Button } from "@payloadcms/ui";
 import { useAllFormFields, useDocumentInfo } from '@payloadcms/ui'
 import { getSummaryFromOpenAi } from "./server";
@@ -9,41 +10,50 @@ import { useForm } from "@payloadcms/ui";
 const GenerateSummaryButton = () => {
   const [, dispatchFields] = useAllFormFields();
   const { setModified } = useForm();
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const doc = useDocumentInfo();
 
 
   return (
     <Button
+      disabled={isLoading}
       onClick={async () => {
-        dispatchFields({
-          type: 'REMOVE',
-          path: 'summary',
-        })
+        try {
+          setIsLoading(true)
 
-        let content = '';
-        const rteContent: DefaultTypedEditorState = doc.savedDocumentData?.content;
-
-        rteContent.root.children.forEach((child) => {
-          const children = child.children;
-          children?.forEach((childchild: any) => {
-            if (childchild.type === 'text') {
-              content += childchild.text;
-            }
+          dispatchFields({
+            type: 'REMOVE',
+            path: 'summary',
           })
-          
-        })
-
-        const summary = await getSummaryFromOpenAi(content);
-
-        dispatchFields({
-          type: 'UPDATE',
-          path: 'summary',
-          value: summary,
-        })
-
-        setModified(true);
-
+  
+          let content = '';
+          const rteContent: DefaultTypedEditorState = doc.savedDocumentData?.content;
+  
+          rteContent.root.children.forEach((child) => {
+            const children = child.children;
+            children?.forEach((childchild: any) => {
+              if (childchild.type === 'text') {
+                content += childchild.text;
+              }
+            })
+            
+          })
+  
+          const summary = await getSummaryFromOpenAi(content);
+  
+          dispatchFields({
+            type: 'UPDATE',
+            path: 'summary',
+            value: summary,
+          })
+  
+          setModified(true);
+          setIsLoading(false);
+  
+        } catch {
+          setIsLoading(false)
+        }
       }}
     >
       AI Summary
