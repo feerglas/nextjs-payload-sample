a3acdbb73d4be2de4d26e42439f2b266f311445b
Yves Tscherry <info@feerglas.com>
2025-02-17 20:40:50 +0100

    add new icons

--- src/components/admin-graphics/icon.tsx
+++ src/components/admin-graphics/icon.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const AdminLogo: React.FC = () => {
   return (
     <div>
-      <Image src='/favicon.svg' alt='SAGW Logo' width='100' height='100' />
+      <Image src='/favicon.svg' alt='SAGW Logo' width='160' height='160' />
     </div>
   )
 }
--- src/components/admin-graphics/logo.tsx
+++ src/components/admin-graphics/logo.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const AdminLogo: React.FC = () => {
   return (
     <div>
-      <Image src='/favicon-alt.svg' alt='SAGW Logo' width='100' height='100' />
+      <Image src='/favicon-alt.svg' alt='SAGW Logo' width='300' height='100' />
     </div>
   )
 }
