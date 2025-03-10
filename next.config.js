ccbe4f0ec72f60b1f57dced6234e954782c580ea
Yves Tscherry <info@feerglas.com>
2025-03-10 17:28:25 +0100

    try to fix public url for netlify

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+// const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+//   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+//   : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+
+const NEXT_PUBLIC_SERVER_URL = process.env.URL || process.env.DEPLOY_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
