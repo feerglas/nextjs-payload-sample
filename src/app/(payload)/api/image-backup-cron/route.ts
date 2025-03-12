40a1ef59be5cdc5770bea37818bc289d4d9eb0b7
Yves Tscherry <info@feerglas.com>
2025-03-12 16:11:49 +0100

    test cron

--- src/app/(payload)/api/image-backup-cron/route.ts
+++ src/app/(payload)/api/image-backup-cron/route.ts
@@ -1,22 +1,53 @@
-// import { NextResponse } from 'next/server';
-
-// export async function GET() {
-//   if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-//     return res.status(401).end('Unauthorized');
-//   }
-
-//   return NextResponse.json({ ok: true });
-// }
-
-import type { NextRequest } from 'next/server';
+import { Readable } from "node:stream";
+import { S3Client } from "@aws-sdk/client-s3";
+import { list } from "@vercel/blob";
+import { Upload } from "@aws-sdk/lib-storage";
+import type { NextRequest } from "next/server";
+import type { ReadableStream } from "node:stream/web";
  
-export function GET(request: NextRequest) {
-  const authHeader = request.headers.get('authorization');
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response('Unauthorized', {
+    return new Response("Unauthorized", {
       status: 401,
     });
   }
  
-  return Response.json({ success: true });
+  const s3 = new S3Client({
+    region: "us-east-1",
+  });
+ 
+  let cursor: string | undefined;
+ 
+  do {
+    const listResult = await list({
+      cursor,
+      limit: 250,
+    });
+ 
+    if (listResult.blobs.length > 0) {
+      await Promise.all(
+        listResult.blobs.map(async (blob) => {
+          const res = await fetch(blob.url);
+          if (res.body) {
+            const parallelUploads3 = new Upload({
+              client: s3,
+              params: {
+                Bucket: 'test-db-backup-aws',
+                Key: blob.pathname,
+                Body: Readable.fromWeb(res.body as ReadableStream),
+              },
+              leavePartsOnError: false,
+            });
+ 
+            await parallelUploads3.done();
+          }
+        })
+      );
+    }
+ 
+    cursor = listResult.cursor;
+  } while (cursor);
+ 
+  return new Response("Backup done!");
 }
